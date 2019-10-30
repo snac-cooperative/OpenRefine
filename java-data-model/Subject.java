33a5122a8ff7497d10c5abdee4621eafe1653ddc
John Perez <91397jrperez@gmail.com>
2019-10-30 02:15:16 -0400

    Added java-data-model folder

---
+++ java-data-model/Subject.java
@@ -0,0 +1,36 @@
+
+
+/**
+ * Subject Class
+ *
+ *
+ * License:
+ *
+ *
+ * @author Robbie Hott
+ * @license https://opensource.org/licenses/BSD-3-Clause BSD 3-Clause
+ * @copyright 2015 the Rector and Visitors of the University of Virginia, and
+ *            the Regents of the University of California
+ */
+package org.snaccooperative.data;
+
+/**
+ * Subject Class
+ *
+ * Class that holds Subject Information
+ *
+ * @author Robbie Hott
+ */
+public class Subject extends AbstractTermData {
+
+    /**
+     * Constructor
+     */
+    public Subject() {
+        super();
+        this.setDataType("Subject");
+        this.setMaxDateCount(0);
+    }
+
+
+}

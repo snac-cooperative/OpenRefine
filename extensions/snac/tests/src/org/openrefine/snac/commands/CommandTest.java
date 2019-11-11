bed36be7b0e0ce1b928601b675a13620d3e48d77
Victor Shen <vs8pd@virginia.edu>
2019-11-11 01:32:51 -0500

    fixed testing issues

--- extensions/snac/tests/src/org/openrefine/snac/commands/CommandTest.java
+++ extensions/snac/tests/src/org/openrefine/snac/commands/CommandTest.java
@@ -1,3 +1,4 @@
+
 /*******************************************************************************
  * MIT License
  *
@@ -33,24 +34,31 @@ import java.io.StringWriter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//import org.openrefine.snac.testing.TestingData2;
+import org.openrefine.snac.testing.TestingData2;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-// import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeMethod;
 
-// import com.google.refine.commands.Command;
-// import com.google.refine.model.Project;
-// import com.google.refine.tests.RefineTest;
+import com.google.refine.commands.Command;
+import com.google.refine.model.Project;
+import com.google.refine.tests.RefineTest;
 
 import org.apache.http.*;
 import org.apache.http.util.EntityUtils;
 import org.apache.http.entity.StringEntity;
-// import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
-// import java.io.IOException;
+import java.io.IOException;
 
-public class CommandTest {
+public class CommandTest extends RefineTest{
+
+    protected Project project = null;
+    protected HttpServletRequest request = null;
+    protected HttpServletResponse response = null;
+    protected StringWriter writer = null;
+    protected Command command = null;
+
 
     @Test
     public void testRecentlyPublished() throws Exception{
@@ -83,6 +91,49 @@ public class CommandTest {
       Assert.assertTrue(result.contains("name_entry"));
     }
 
+    /*
+    * Test API calls for read_resource
+    */
+
+    @Test
+    public void testReadConstellation() throws Exception{
+      DefaultHttpClient client = new DefaultHttpClient();
+      HttpPost post = new HttpPost("http://api.snaccooperative.org");
+      post.setEntity(new StringEntity("{\"command\": \"read_resource\",\"resourceid\": 7149468}","UTF-8"));
+      HttpResponse response = client.execute(post);
+      String result = EntityUtils.toString(response.getEntity());
+      Assert.assertTrue(result.contains("resource"));
+    }
+
+    @Test
+    public void TestNonexistantReadResource() throws Exception{
+      DefaultHttpClient client = new DefaultHttpClient();
+      HttpPost post = new HttpPost("http://api.snaccooperative.org");
+      post.setEntity(new StringEntity("{\"command\": \"read_resource\",\"resourceid\": \"100000000\"}", "UTF-8"));
+      HttpResponse response = client.execute(post);
+      String result = EntityUtils.toString(response.getEntity());
+      Assert.assertTrue(!result.contains("dataType\": \"Resource\""));
+    }
+
+    /*
+    * Test API calls for resource_search
+    */
+
+    @Test
+    public void TestResourceSearch() throws Exception{
+      DefaultHttpClient client = new DefaultHttpClient();
+      HttpPost post = new HttpPost("http://api.snaccooperative.org");
+      post.setEntity(new StringEntity("{\"command\": \"resource_search\",\"term\": \"Papers\"}", "UTF-8"));
+      HttpResponse response = client.execute(post);
+      String result = EntityUtils.toString(response.getEntity());
+      Assert.assertTrue(result.contains("total\": ")); // we got a result
+      Assert.assertTrue(!result.contains("total\": 0,")); // There were resources found
+    }
+
+    /*
+    * Test API calls for read
+    */
+
     @Test
     public void testSearchConcepts() throws Exception{
       DefaultHttpClient client = new DefaultHttpClient();
@@ -94,15 +145,9 @@ public class CommandTest {
       Assert.assertTrue(result.contains("500"));
     }
 
-    @Test
-    public void testReadResource() throws Exception{
-      DefaultHttpClient client = new DefaultHttpClient();
-      HttpPost post = new HttpPost("http://api.snaccooperative.org");
-      post.setEntity(new StringEntity("{\"command\": \"read_resource\",\"resourceid\": 7149468}","UTF-8"));
-      HttpResponse response = client.execute(post);
-      String result = EntityUtils.toString(response.getEntity());
-      Assert.assertTrue(result.contains("resource"));
-    }
+    /*
+    * Test API calls for constellation_history
+    */
 
     @Test
     public void testConstellationHistory() throws Exception{
@@ -112,6 +157,63 @@ public class CommandTest {
       HttpResponse response = client.execute(post);
       String result = EntityUtils.toString(response.getEntity());
       Assert.assertTrue(result.contains("Constellation"));
+    }
+
+    /*
+    * Test API calls for shared_resources
+    */
+
+   @Test
+    public void testSharedResources() throws Exception{
+      DefaultHttpClient client = new DefaultHttpClient();
+      HttpPost post = new HttpPost("http://api.snaccooperative.org");
+      post.setEntity(new StringEntity("{\"command\": \"shared_resources\",\"icid1\": 29260863 ,\"icid2\": 50307952}","UTF-8"));
+      HttpResponse response = client.execute(post);
+      String result = EntityUtils.toString(response.getEntity());
+      Assert.assertTrue(result.contains("7960925"));
+    }
+
+    /*
+    * Test API calls for read_vocabulary
+    */
+
+    @Test
+    public void testReadVocabulary() throws Exception{
+      DefaultHttpClient client = new DefaultHttpClient();
+      HttpPost post = new HttpPost("http://api.snaccooperative.org");
+      post.setEntity(new StringEntity("{\"command\": \"read_vocabulary\",\"term_id\": 700}","UTF-8"));
+      HttpResponse response = client.execute(post);
+      String result = EntityUtils.toString(response.getEntity());
+      Assert.assertTrue(result.contains("person"));
+    }
+
+    /*
+    * Test API calls for get_holdings
+    */
+
+    @Test
+    public void testGetHoldings() throws Exception{
+      DefaultHttpClient client = new DefaultHttpClient();
+      HttpPost post = new HttpPost("http://api.snaccooperative.org");
+      post.setEntity(new StringEntity("{\"command\": \"get_holdings\",\"constellationid\": 76778184}","UTF-8"));
+      HttpResponse response = client.execute(post);
+      String result = EntityUtils.toString(response.getEntity());
+      Assert.assertTrue(result.contains("7677119"));
+    }
+
+    /*
+    * Test API calls for elastic
+    */
+
+    @Test
+    public void testElastic() throws Exception{
+      DefaultHttpClient client = new DefaultHttpClient();
+      HttpPost post = new HttpPost("http://api.snaccooperative.org");
+      post.setEntity(new StringEntity("{\"command\": \"elastic\",\"query\": { \"simple_query_string\": { \"query\": \"poets\", \"default_operator\": \"and\"}}, \"size\": 2, \"from\":0}","UTF-8"));
+      HttpResponse response = client.execute(post);
+      String result = EntityUtils.toString(response.getEntity());
+      Assert.assertTrue(result.contains("\"result\": \"success\""));
+      Assert.assertTrue(!result.contains("total\": 0,"));
     }
 
     @Test
@@ -143,22 +245,28 @@ public class CommandTest {
         String result = EntityUtils.toString(response.getEntity());
         Assert.assertTrue(result.contains("40081682"));
     }
-  /*  @BeforeMethod(alwaysRun = true)
+/*
+    @BeforeMethod(alwaysRun = true)
     public void setUpProject() {
-        project = createCSVProject(TestingData.inceptionWithNewCsv);
-        TestingData.reconcileInceptionCells(project);
+        project = createCSVProject(TestingData2.inceptionWithNewCsv);
+        TestingData2.reconcileInceptionCells(project);
         request = mock(HttpServletRequest.class);
         response = mock(HttpServletResponse.class);
         writer = new StringWriter();
         PrintWriter printWriter = new PrintWriter(writer);
-
         when(request.getParameter("project")).thenReturn(String.valueOf(project.id));
-
         try {
             when(response.getWriter()).thenReturn(printWriter);
         } catch (IOException e1) {
             Assert.fail();
         }
     }*/
+
+    @Test
+    public void testProjectColumns() throws Exception{
+      project = createCSVProject(TestingData2.inceptionWithNewCsv);
+      //project = createProjectWithColumns("test_columns", TestingData2.column_values);
+      Assert.assertEquals(project.columnModel.getColumnNames().size(), 3);
+    }
 
 }
--- extensions/snac/tests/src/org/openrefine/snac/testing/TestingData2.java
+++ extensions/snac/tests/src/org/openrefine/snac/testing/TestingData2.java
@@ -22,11 +22,12 @@
  * SOFTWARE.
  ******************************************************************************/
 package org.openrefine.snac.testing;
-/*import java.io.IOException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collections;
 
 import org.apache.commons.io.IOUtils;
+/*
 import org.openrefine.wikidata.schema.WbLanguageConstant;
 import org.openrefine.wikidata.schema.WbMonolingualExpr;
 import org.openrefine.wikidata.schema.WbStringConstant;
@@ -38,13 +39,15 @@ import org.wikidata.wdtk.datamodel.interfaces.ItemIdValue;
 import org.wikidata.wdtk.datamodel.interfaces.PropertyIdValue;
 import org.wikidata.wdtk.datamodel.interfaces.Statement;
 import org.wikidata.wdtk.datamodel.interfaces.StatementRank;
-
+*/
 import com.google.refine.model.Cell;
 import com.google.refine.model.Project;
 import com.google.refine.model.Recon;
 import com.google.refine.model.ReconCandidate;
 
 public class TestingData2 {
+
+    public static final String[] column_values = {"col1", "col2", "col3"};
 
     public static final String inceptionCsv = "subject,inception,reference\n"
             + "Q1377,1919,http://www.ljubljana-slovenia.com/university-ljubljana\n" + "Q865528,1965,";
@@ -56,20 +59,17 @@ public class TestingData2 {
             + "\tS813\t+2018-02-28T00:00:00Z/11\n" + "Q865528\tP571\t+1965-01-01T00:00:00Z/9"
             + "\tS813\t+2018-02-28T00:00:00Z/11\n" + "CREATE\n" + "LAST\tP571\t+2016-01-01T00:00:00Z/9"
             + "\tS854\t\"http://new-uni.com/\"" + "\tS813\t+2018-02-28T00:00:00Z/11\n";
-
+/*
     public static ItemIdValue newIdA = makeNewItemIdValue(1234L, "new item A");
     public static ItemIdValue newIdB = makeNewItemIdValue(4567L, "new item B");
     public static ItemIdValue matchedId = makeMatchedItemIdValue("Q89", "eist");
     public static ItemIdValue existingId = Datamodel.makeWikidataItemIdValue("Q43");
-
     protected static PropertyIdValue pid = Datamodel.makeWikidataPropertyIdValue("P38");
-
     public static class ReconStub extends Recon {
         public ReconStub(long id, long judgmentHistoryEntry) {
             super(id, judgmentHistoryEntry);
         }
     }
-
     public static Recon makeNewItemRecon(long id) {
         Recon recon = new ReconStub(id, 382398L); // we keep the same judgment id because it is ignored
         recon.identifierSpace = "http://www.wikidata.org/entity/";
@@ -77,7 +77,6 @@ public class TestingData2 {
         recon.judgment = Recon.Judgment.New;
         return recon;
     }
-
     public static Recon makeMatchedRecon(String qid, String name, String[] types) {
         Recon recon = Recon.makeWikidataRecon(123456L);
         recon.match = new ReconCandidate(qid, name, types, 100.0);
@@ -85,59 +84,47 @@ public class TestingData2 {
         recon.judgment = Recon.Judgment.Matched;
         return recon;
     }
-
     public static Recon makeMatchedRecon(String qid, String name) {
         return makeMatchedRecon(qid, name, new String[0]);
     }
-
     public static Cell makeNewItemCell(long judgementId, String name) {
         return new Cell(name, makeNewItemRecon(judgementId));
     }
-
     public static Cell makeMatchedCell(String qid, String name) {
         return new Cell(name, makeMatchedRecon(qid, name));
     }
-
     public static ReconItemIdValue makeNewItemIdValue(long judgementId, String name) {
         return new ReconItemIdValue(makeNewItemRecon(judgementId), name);
     }
-
     public static ReconItemIdValue makeMatchedItemIdValue(String qid, String name) {
         return new ReconItemIdValue(makeMatchedRecon(qid, name), name);
     }
-
     public static ReconPropertyIdValue makeNewPropertyIdValue(long judgmentId, String name) {
         return new ReconPropertyIdValue(makeNewItemRecon(judgmentId), name);
     }
-
     public static ReconPropertyIdValue makeMatchedPropertyIdValue(String pid, String name) {
         return new ReconPropertyIdValue(makeMatchedRecon(pid, name), name);
     }
-
     public static WbMonolingualExpr getTestMonolingualExpr(String langCode, String langLabel, String text) {
         return new WbMonolingualExpr(new WbLanguageConstant(langCode, langLabel), new WbStringConstant(text));
     }
-
     public static Statement generateStatement(ItemIdValue from, PropertyIdValue pid, ItemIdValue to) {
         Claim claim = Datamodel.makeClaim(from, Datamodel.makeValueSnak(pid, to), Collections.emptyList());
         return Datamodel.makeStatement(claim, Collections.emptyList(), StatementRank.NORMAL, "");
     }
-
     public static Statement generateStatement(ItemIdValue from, ItemIdValue to) {
         return generateStatement(from, pid, to);
     }
-
     public static String jsonFromFile(String filename)
             throws IOException {
         InputStream f = TestingData.class.getClassLoader().getResourceAsStream(filename);
         String decoded = IOUtils.toString(f);
         return decoded.trim();
     }
-
     public static void reconcileInceptionCells(Project project) {
         project.rows.get(0).cells.set(0, TestingData.makeMatchedCell("Q1377", "University of Ljubljana"));
         project.rows.get(1).cells.set(0, TestingData.makeMatchedCell("Q865528", "University of Warwick"));
         project.rows.get(2).cells.set(0, TestingData.makeNewItemCell(1234L, "new uni"));
     }
-
-}*/
+*/
+}
